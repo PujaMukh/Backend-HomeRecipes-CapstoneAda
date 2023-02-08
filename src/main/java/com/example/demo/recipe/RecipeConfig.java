1cf791e7af992ad21ea97a7b1521b4cef6dd181d
Puja Mukherjee <pujam836@gmail.com>
2023-02-07 22:06:52 -0800

    deleted hardcoded recipes in bean

--- src/main/java/com/example/demo/recipe/RecipeConfig.java
+++ src/main/java/com/example/demo/recipe/RecipeConfig.java
@@ -12,33 +12,33 @@ public class RecipeConfig {
     @Bean
     CommandLineRunner commandLineRunner(RecipeRepository repository) {
         return args -> {
-            Recipe bengali1 = new Recipe (
-                        "mutton curry",
-                        "Puja Mukherjee",
-                        Instant.now().toEpochMilli(),
-
-                        Cuisine.INDIAN_BENGALI,
-                        MealType.LUNCH,
-                        "http://google.com",
-                        List.of("mutton", "turmeric", "salt"),
-                        List.of("Marinate the mutton with salt, turmeric. Cut onion and saute in pressure cooker, add masalas. Put 9 whistles in cooker")
-
-                );
-            Recipe bengali2 = new Recipe (
-                    "bandhakopi",
-                    "Puja Mukherjee",
-                    Instant.now().toEpochMilli(),
-
-                    Cuisine.INDIAN_BENGALI,
-                    MealType.DINNER,
-                    "http://google.com",
-                    List.of("cabbage", "turmeric", "salt", "sugar", "garam masala"),
-                    List.of("Marinate the mutton with salt, turmeric. Cut onion and saute in pressure cooker, add masalas. Put 9 whistles in cooker")
-
-            );
-            repository.saveAll(
-                    List.of(bengali1, bengali2)
-            );
+//            Recipe bengali1 = new Recipe (
+//                        "mutton curry",
+//                        "Puja Mukherjee",
+//                        Instant.now().toEpochMilli(),
+//
+//                        Cuisine.INDIAN_BENGALI,
+//                        MealType.LUNCH,
+//                        "http://google.com",
+//                        List.of("mutton", "turmeric", "salt"),
+//                        List.of("Marinate the mutton with salt, turmeric. Cut onion and saute in pressure cooker, add masalas. Put 9 whistles in cooker")
+//
+//                );
+//            Recipe bengali2 = new Recipe (
+//                    "bandhakopi",
+//                    "Puja Mukherjee",
+//                    Instant.now().toEpochMilli(),
+//
+//                    Cuisine.INDIAN_BENGALI,
+//                    MealType.DINNER,
+//                    "http://google.com",
+//                    List.of("cabbage", "turmeric", "salt", "sugar", "garam masala"),
+//                    List.of("Marinate the mutton with salt, turmeric. Cut onion and saute in pressure cooker, add masalas. Put 9 whistles in cooker")
+//
+//            );
+//            repository.saveAll(
+//                    List.of(bengali1, bengali2)
+//            );
 
 
         };
